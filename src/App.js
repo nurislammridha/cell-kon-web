cab6e1d61adaa3389f52f49934cb496dc260d57a
nurislammridha <mridha851@gmail.com>
2024-03-14 13:41:03 +0600

    Update

--- src/App.js
+++ src/App.js
@@ -25,6 +25,7 @@ import pro1 from './assets/images/other/pro1.jpg'
 import pro2 from './assets/images/other/pro2.jpg'
 import pro3 from './assets/images/other/pro3.jpg'
 import OwlCarousel from "react-owl-carousel";
+import Select from "react-select"
 function App() {
   let option = {
     responsive: {
@@ -100,7 +101,7 @@ function App() {
         <div className='content'>
           <div className='w-1176'>
             {/* home page  */}
-            <div className='hero_main'>
+            {/* <div className='hero_main'>
               <OwlCarousel
                 className="owl-theme"
                 loop
@@ -326,36 +327,262 @@ function App() {
 
               </div>
 
-            </div>
+            </div> */}
             {/* details page */}
             {/* <div className='details_page'>
-<div className='left'>
-  <div className='image'></div>
-  <div className='images'>
-    <img src={pro1} alt='product'/>
-    <img src={pro1} alt='product'/>
-    <img src={pro1} alt='product'/>
-    <img src={pro1} alt='product'/>
-  </div>
-</div>
-<div className='right'>
-  <div className='title_section'>
-    <div>Yamaha yZF R15 df er rtt rtt rtt frt rtg rrt</div>
-    <div className='share'>
-    <i class="fa fa-share-alt" aria-hidden="true"></i>
-    </div>
-  </div>
-  <div className='brand'>Brand: YAMAHA</div>
-  <div className='sold_by'>Sild By: YAMAHA Official</div>
-  <div className='del_price'>&#2547;4,50990</div>
-  <div className='sell_price'>&#2547;4,50990</div>
-  <div className='txt_cq'>Color</div>
-  <div className='colors'>
-   {[1,2,3,4].map()} <a href>Black</a>
-  </div>
-  <div className='txt_cq'>Color</div>
-</div>
+              <div className='main'>
+                <div className='left'>
+                  <div className='image'>
+                    <img src={pro3} alt='product img' />
+                  </div>
+                  <div className='parent_img'>
+                    <div className='images'>
+                      {[1, 2, 3, 4].map((item) => (<img src={pro3} alt='product' />))}
+                    </div>
+                    <div className='arrow'>
+                      <div className='left_arrow'><i class='fas fa-chevron-left'></i></div>
+                      <div className='right_arrow'><i class='fas fa-chevron-right'></i></div>
+                    </div>
+                  </div>
+                </div>
+                <div className='right'>
+                  <div className='title_section'>
+                    <div className='txt'>Yamaha yZF R15 df er rtt rtt rtt frt rtg rrt</div>
+                    <div className='share'>
+                      <i class="fa fa-share-alt" aria-hidden="true"></i>
+                    </div>
+                  </div>
+                  <div className='brand'>Brand: YAMAHA</div>
+                  <div className='sold_by'>
+                    <span>Sold By: YAMAHA Official</span>
+                    <a>Visit Store</a>
+                  </div>
+                  <div className='del_price'>&#2547;4,50990</div>
+                  <div className='product_price'>&#2547;4,50990</div>
+                  <div className='txt_cq'>Color</div>
+                  <div className='colors'>
+                    {["#f00", "#0f0", "#00f", "#ff0"].map((item) => (<a href style={{ backgroundColor: item }}>Black</a>))}
+                  </div>
+                  <div className='txt_cq'>Quantity</div>
+                  <div className='quantity_button'>
+                    <div className='btn plus'><i class="fa fa-plus"></i></div>
+                    <div className='btn number'>1</div>
+                    <div className='btn minus'><i class="fa fa-minus"></i></div>
+                  </div>
+                  <div className='btn_buy'>
+                    <a href className='btn cart'>Add to Cart</a>
+                    <a href className='btn buy'>Buy Now</a>
+                  </div>
+                  <div className='have_question'>Have questions about this product</div>
+                  <div className='call'>
+                    <i class="fa fa-phone" aria-hidden="true"></i>
+                    <span>+8801784528799</span>
+                  </div>
+                </div>
+              </div>
+              <div className='short_details'>
+                <h2>Short Details</h2>
+                <div className='txt'>
+                  lorem ipsum lorem ipsum lorem ipsum
+                  lorem ipsum lorem ipsum lorem ipsum
+                  lorem ipsum lorem ipsum lorem ipsum
+                  lorem ipsum lorem ipsum lorem ipsum
+                  lorem ipsum lorem ipsum lorem ipsum
+                  lorem ipsum lorem ipsum lorem ipsum
+                </div>
+                <a className='view_more'>View More</a>
+              </div>
+              <div className='full_details'>
+                <h2>Full Details</h2>
+                <div className='txt'>
+                  lorem ipsum lorem ipsum lorem ipsum
+                  lorem ipsum lorem ipsum lorem ipsum
+                  lorem ipsum lorem ipsum lorem ipsum
+                  lorem ipsum lorem ipsum lorem ipsum
+                  lorem ipsum lorem ipsum lorem ipsum
+                  lorem ipsum lorem ipsum lorem ipsum
+                  lorem ipsum lorem ipsum lorem ipsum
+                  lorem ipsum lorem ipsum lorem ipsum
+                  lorem ipsum lorem ipsum lorem ipsum
+                  lorem ipsum lorem ipsum lorem ipsum
+                  lorem ipsum lorem ipsum lorem ipsum
+                  lorem ipsum lorem ipsum lorem ipsum
+                  lorem ipsum lorem ipsum lorem ipsum
+                  lorem ipsum lorem ipsum lorem ipsum
+                  lorem ipsum lorem ipsum lorem ipsum
+                  lorem ipsum lorem ipsum lorem ipsum
+                  lorem ipsum lorem ipsum lorem ipsum
+                  lorem ipsum lorem ipsum lorem ipsum
+                  lorem ipsum lorem ipsum lorem ipsum
+                  lorem ipsum lorem ipsum lorem ipsum
+                  lorem ipsum lorem ipsum lorem ipsum
+                  lorem ipsum lorem ipsum lorem ipsum
+                  lorem ipsum lorem ipsum lorem ipsum
+                  lorem ipsum lorem ipsum lorem ipsum
+                  lorem ipsum lorem ipsum lorem ipsum
+                  lorem ipsum lorem ipsum lorem ipsum
+                  lorem ipsum lorem ipsum lorem ipsum
+                  lorem ipsum lorem ipsum lorem ipsum
+                  lorem ipsum lorem ipsum lorem ipsum
+                  lorem ipsum lorem ipsum lorem ipsum
+                </div>
+              </div>
+              <div className='home_all_products'>
+                <div className='all_title'>
+                  <h2 className='title'>Related Products</h2>
+                  <a className='view_all' href=''>View All</a>
+                </div>
+                <div className='products'>
+
+                  {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 13, 14, 15, 16, 17].map((item, index) => (
+                    <div className="product_cart">
+                      <div>
+                        <div className='product_img'>
+                          <img
+                            src={pro3}
+                            alt="product"
+                          />
+                        </div>
+                        <div className='product_name'>
+                          he llo bi ke dff fe r te s fg rd f d sf df ... ... ..
+                        </div>
+                      </div>
+                      <div>
+                        <div className='del_price'>
+                          &#2547;9,9990
+                        </div>
+                        <div className='product_price'>
+                          &#2547;8,990
+                        </div>
+                      </div>
+                    </div>
+                  ))}
+
+                </div>
+
+              </div>
             </div> */}
+            {/* Product page */}
+            <div className='product_page'>
+              <div className='order_section'>
+                <div className='showing'>Showing 250 Product</div>
+                <div className='select'>
+                  <div className='sort'>Short By</div>
+                  <div className='price'>
+                    <Select
+                      options={[{ label: "hi", value: 1 }, { label: "hqw", value: 2 }]}
+                      name="price"
+                      value={{ label: "hi" }}
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className='filter_product'>
+                <div className='filter_left'>
+                  <div className='filter'>
+                    <img src={categoryIcon} />
+                    <span>Filter</span>
+                  </div>
+                  <div className='category'>
+                    <div className='txt'>
+                      Category
+                    </div>
+                    <ul>
+                      <li>
+                        <input type='radio' />
+                        <span>Smartphone</span>
+                      </li>
+                      <li>
+                        <input type='radio' />
+                        <span>Smartphone</span>
+                      </li>
+                      <li>
+                        <input type='radio' />
+                        <span>Smartphone</span>
+                      </li>
+                      <li>
+                        <input type='radio' />
+                        <span>Smartphone</span>
+                      </li>
+                      <li>
+                        <input type='radio' />
+                        <span>Smartphone</span>
+                      </li>
+                    </ul>
+                  </div>
+                  <div className='category'>
+                    <div className='txt'>
+                      Shops
+                    </div>
+                    <ul>
+                      <li>
+                        <input type='radio' />
+                        <span>Smartphone</span>
+                      </li>
+                      <li>
+                        <input type='radio' />
+                        <span>Smartphone</span>
+                      </li>
+                      <li>
+                        <input type='radio' />
+                        <span>Smartphone</span>
+                      </li>
+                      <li>
+                        <input type='radio' />
+                        <span>Smartphone</span>
+                      </li>
+                      <li>
+                        <input type='radio' />
+                        <span>Smartphone</span>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+                <div className='filter_right'>
+                  <div className='products'>
+                    <div className='home_all_products product_form_page'>
+                      <div className='products'>
+
+                        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16].map((item, index) => (
+                          <div className="product_cart">
+                            <div>
+                              <div className='product_img'>
+                                <img
+                                  src={pro3}
+                                  alt="product"
+                                />
+                              </div>
+                              <div className='product_name'>
+                                he llo bi ke dff fe r te s fg rd f d sf df ... ... ..
+                              </div>
+                            </div>
+                            <div>
+                              <div className='del_price'>
+                                &#2547;9,9990
+                              </div>
+                              <div className='product_price'>
+                                &#2547;8,990
+                              </div>
+                            </div>
+                          </div>
+                        ))}
+
+                      </div>
+                      <div className='pagination'>
+                        <div className='item active'>1</div>
+                        <div className='item'>2</div>
+                        <div className='item'>3</div>
+                        <div className='item'>4</div>
+                        <div className='item dots'>.......</div>
+                        <div className='item'>25</div>
+                        <div className='item next'>Next</div>
+                      </div>
+                    </div>
+                  </div>
+
+                </div>
+              </div>
+            </div>
           </div>
         </div>
         <div className='footer'>
