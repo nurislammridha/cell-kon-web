50dc8531f6abf2cd438e340e88c9eb29f815a79f
nurislammridha <mridha851@gmail.com>
2024-03-18 21:28:53 +0600

    login sign up

--- src/modules/pages/ShopProductsPage.js
+++ src/modules/pages/ShopProductsPage.js
@@ -9,34 +9,36 @@ import starFillIcon from "../../assets/images/icons/startFill.png"
 const ShopProductsPage = () => {
     return (
         <>
-            <div className='product_page'>
-                {/* order section */}
-                <div className='shop'>
-                    <img src={shopIcon} alt='shop icon' />
-                    <div className='shop_info'>
-                        <h1>Nova Enterprise For Flash Sale COD</h1>
-                        <h3 className='mt13'>Delivery in 1-5 Days</h3>
-                        <div className='star mt13'>
-                            <img src={starFillIcon} />
-                            <img src={starFillIcon} />
-                            <img src={starFillIcon} />
-                            <img src={starFillIcon} />
-                            <img src={starIcon} />
-                            <h3>(1.8K)</h3>
+            <div className='shop_container'>
+                <div className='product_page'>
+                    {/* order section */}
+                    <div className='shop'>
+                        <img src={shopIcon} alt='shop icon' />
+                        <div className='shop_info'>
+                            <h1>Nova Enterprise For Flash Sale COD</h1>
+                            <h3 className='mt13'>Delivery in 1-5 Days</h3>
+                            <div className='star mt13'>
+                                <img src={starFillIcon} />
+                                <img src={starFillIcon} />
+                                <img src={starFillIcon} />
+                                <img src={starFillIcon} />
+                                <img src={starIcon} />
+                                <h3>(1.8K)</h3>
+                            </div>
+                            <h3 className='mt13'>Savar, Dhaka, Dhaka</h3>
                         </div>
-                        <h3 className='mt13'>Savar, Dhaka, Dhaka</h3>
                     </div>
-                </div>
-                <div className='filter_product'>
-                    {/* Shops  */}
-                    <Filter />
-                    <div className='filter_right'>
-                        <div className='products'>
-                            <div className='home_all_products product_form_page'>
-                                {/* All products */}
-                                <AllProducts />
-                                {/* //pagination */}
-                                <ProductsPagination />
+                    <div className='filter_product'>
+                        {/* Shops  */}
+                        <Filter />
+                        <div className='filter_right'>
+                            <div className='products'>
+                                <div className='home_all_products product_form_page'>
+                                    {/* All products */}
+                                    <AllProducts />
+                                    {/* //pagination */}
+                                    <ProductsPagination />
+                                </div>
                             </div>
                         </div>
                     </div>
